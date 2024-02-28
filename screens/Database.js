5adfb50825b8644b95ff17e34f4ed79efba9ea1d
NathanJargon <metamorphiaSH@gmail.com>
2024-02-28 10:59:13 +0800

    major imp

--- screens/Database.js
+++ screens/Database.js
@@ -3,17 +3,111 @@ import * as SQLite from 'expo-sqlite';
 const db = SQLite.openDatabase('posts.db');
 
 export const setupDatabase = async () => {
-    return new Promise((resolve, reject) => {
-      db.transaction(tx => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'create table if not exists posts (id integer primary key not null, userName text, time text, title text, description text, image text, likeCount integer, dislikeCount integer, commentCount integer);',
+        [],
+        resolve,
+        (_, error) => reject(error)
+      );
+      tx.executeSql(
+        'create table if not exists comments (id integer primary key not null, text text, createdAt text, postId integer, FOREIGN KEY(postId) REFERENCES posts(id));',
+        [],
+        resolve,
+        (_, error) => reject(error)
+      );
+      tx.executeSql(
+        'create table if not exists likes (id integer primary key not null, userId integer, postId integer, FOREIGN KEY(postId) REFERENCES posts(id));',
+        [],
+        resolve,
+        (_, error) => reject(error)
+      );
+      tx.executeSql(
+        'create table if not exists dislikes (id integer primary key not null, userId integer, postId integer, FOREIGN KEY(postId) REFERENCES posts(id));',
+        [],
+        resolve,
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
+
+export const updateLikes = async (postId, userId, like) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      if (like) {
         tx.executeSql(
-          'create table if not exists posts (id integer primary key not null, userName text, time text, title text, description text, image text);',
-          [],
-          resolve,
+          'insert into likes (userId, postId) values (?, ?)',
+          [userId, postId],
+          (_, result) => resolve(result),
           (_, error) => reject(error)
         );
-      });
+      } else {
+        tx.executeSql(
+          'delete from likes where userId = ? and postId = ?',
+          [userId, postId],
+          (_, result) => resolve(result),
+          (_, error) => reject(error)
+        );
+      }
     });
-  };
+  });
+};
+
+export const updateDislikes = async (postId, userId, dislike) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      if (dislike) {
+        tx.executeSql(
+          'insert into dislikes (userId, postId) values (?, ?)',
+          [userId, postId],
+          (_, result) => resolve(result),
+          (_, error) => reject(error)
+        );
+      } else {
+        tx.executeSql(
+          'delete from dislikes where userId = ? and postId = ?',
+          [userId, postId],
+          (_, result) => resolve(result),
+          (_, error) => reject(error)
+        );
+      }
+    });
+  });
+};
+
+export const getComments = async (postId) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'select * from comments where postId = ?',
+        [postId],
+        (_, { rows: { _array } }) => resolve(_array),
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
+
+export const insertComment = async (comment, postId) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'insert into comments (text, createdAt, postId) values (?, ?, ?)',
+        [comment, new Date().getTime(), postId],
+        (_, result) => resolve(result),
+        (_, error) => reject(error)
+      );
+      tx.executeSql(
+        'update posts set commentCount = commentCount + 1 where id = ?',
+        [postId],
+        (_, result) => resolve(result),
+        (_, error) => reject(error)
+      );
+    });
+  });
+};
 
   export const insertPost = async (post) => {
     return new Promise((resolve, reject) => {
--- screens/Post.js
+++ screens/Post.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions, Modal } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions, Modal, TextInput, ScrollView } from 'react-native';
 import { Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
 import { formatDistanceToNow } from 'date-fns';
+import { insertComment, updateLikes, updateDislikes, getComments } from './Database';
 
 const { width, height } = Dimensions.get('window');
 
@@ -9,6 +10,86 @@ export function Post({ post, onDelete, onEdit }) {
     const [isImageModalVisible, setImageModalVisible] = useState(false);
     const [timeString, setTimeString] = useState('');
     const { width, height } = Dimensions.get('window');
+    const [likes, setLikes] = useState(post.likes || 0);
+    const [dislikes, setDislikes] = useState(post.dislikes || 0);
+    const [likeImage, setLikeImage] = useState(require('../assets/like.png'));
+    const [dislikeImage, setDislikeImage] = useState(require('../assets/dislike.png'));
+    const [isCommentModalVisible, setCommentModalVisible] = useState(false);
+    const [comment, setComment] = useState('');
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+      getComments(post.id)
+        .then(fetchedComments => {
+          setComments(fetchedComments);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }, []);
+
+    const handleComment = () => {
+      if (comment.trim() !== '') {
+        insertComment(comment, post.id)
+          .then(() => {
+            setComment('');
+            getComments(post.id)
+              .then(fetchedComments => {
+                setComments(fetchedComments);
+              })
+              .catch(error => {
+                console.error(error);
+              });
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    };
+    
+    const handleLike = () => {
+      if (likeImage === require('../assets/like.png')) {
+        setLikes(likes + 1);
+        setLikeImage(require('../assets/like-color.png'));
+        updateLikes(post.id, 1)
+          .then(() => {
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      } else {
+        setLikes(likes - 1);
+        setLikeImage(require('../assets/like.png'));
+        updateLikes(post.id, -1)
+          .then(() => {
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    };
+    
+    const handleDislike = () => {
+      if (dislikeImage === require('../assets/dislike.png')) {
+        setDislikes(dislikes + 1);
+        setDislikeImage(require('../assets/dislike-color.png'));
+        updateDislikes(post.id, 1)
+          .then(() => {
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      } else {
+        setDislikes(dislikes - 1);
+        setDislikeImage(require('../assets/dislike.png'));
+        updateDislikes(post.id, -1)
+          .then(() => {
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    };
 
     useEffect(() => {
       const updateTimeString = () => {
@@ -42,52 +123,101 @@ export function Post({ post, onDelete, onEdit }) {
             </MenuOption>
             </MenuOptions>
           </Menu>
-        </View>
-        <Text style={styles.postTitle}>{post.title}</Text>
-        <Text style={styles.postDescription}>{post.description}</Text>
-        <TouchableOpacity onPress={() => {
-        if (post.image) {
-            setImageModalVisible(true);
-        } else {
-            alert('Image is empty, please edit to add a picture.');
-        }
-        }}>
-        <Image 
-            source={post.image ? { uri: post.image } : require('../assets/empty.png')} 
-            style={styles.postImage} 
-        />
-        </TouchableOpacity>
-        <View style={styles.postActions}>
-          <TouchableOpacity>
-            <Image source={require('../assets/like.png')} style={styles.actionIcon} />
+          </View>
+          <Text style={styles.postTitle}>{post.title}</Text>
+          <Text style={styles.postDescription}>{post.description}</Text>
+          <TouchableOpacity onPress={() => {
+            if (post.image) {
+              setImageModalVisible(true);
+            } else {
+              alert('Image is empty, please edit to add a picture.');
+            }
+          }}>
+            <Image 
+              source={post.image ? { uri: post.image } : require('../assets/empty.png')} 
+              style={styles.postImage} 
+            />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Image source={require('../assets/dislike.png')} style={styles.actionIcon} />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.commentButton}>
-            <Text style={styles.actionText}>Comment</Text>
-          </TouchableOpacity>
-        </View>
-        <Modal
-        animationType="slide"
-        transparent={true}
-        visible={isImageModalVisible}
-        onRequestClose={() => {
-            setImageModalVisible(!isImageModalVisible);
-        }}
-        >
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}>
-            <Image source={{ uri: post.image }} style={{ width: '80%', height: '80%' }} resizeMode="contain" />
-            <TouchableOpacity
-            style={{ marginTop: height * 0.02, borderRadius: 20, padding: width * 0.04, backgroundColor: 'white' }}
-            onPress={() => setImageModalVisible(!isImageModalVisible)}
-            >
-            <Text>Close</Text>
+          <View style={styles.postStats}>
+            <Text style={styles.statsText}>{likes} likes</Text>
+            <Text style={styles.statsText}>{dislikes} dislikes</Text>
+            <Text style={styles.statsText}>{post.comments || 0} comments</Text>
+          </View>
+          <View style={styles.lineStyle} />
+          <View style={styles.postActions}>
+            <TouchableOpacity onPress={handleLike}>
+              <Image source={likeImage} style={styles.actionIcon} />
             </TouchableOpacity>
-        </View>
-        </Modal>
-      </View>
-    );
+            <TouchableOpacity onPress={handleDislike}>
+              <Image source={dislikeImage} style={styles.actionIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.commentButton} onPress={() => setCommentModalVisible(true)}>
+              <Text style={styles.actionText}>Comment</Text>
+            </TouchableOpacity>
+          </View>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={isCommentModalVisible}
+            onRequestClose={() => {
+              setCommentModalVisible(!isCommentModalVisible);
+            }}
+          >
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}>
+              <View style={{ width: '80%', height: '80%', backgroundColor: 'white', borderRadius: 20, padding: 20 }}>
+              <ScrollView style={{ flex: 1 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                  <Text>Comments</Text>
+                  <TouchableOpacity onPress={() => setCommentModalVisible(false)}>
+                    <Image
+                      source={require('../assets/arrow_back_modal.png')}
+                      style={{ width: 30, height: 30 }}
+                    />
+                  </TouchableOpacity>
+                </View>
+                {comments.map((comment, index) => (
+                <View key={index} style={styles.commentContainer}>
+                  <Text style={styles.commentUser}>{comment.user || 'User'}:</Text>
+                  <Text style={styles.commentText}>"{comment.text}"</Text>
+                </View>
+              ))}
+              </ScrollView>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                  <TextInput
+                    style={{ flex: 1, height: 40, borderColor: 'gray', borderWidth: 1, borderRadius: 10, padding: 10 }}
+                    onChangeText={text => setComment(text)}
+                    value={comment}
+                  />
+                  <TouchableOpacity onPress={handleComment}>
+                    <Image
+                      source={comment ? require('../assets/send-color.png') : require('../assets/send.png')}
+                      style={{ width: 30, height: 30, margin: 10 }}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={isImageModalVisible}
+            onRequestClose={() => {
+              setImageModalVisible(!isImageModalVisible);
+            }}
+          >
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.5)' }}>
+              <Image source={{ uri: post.image }} style={{ width: '80%', height: '80%' }} resizeMode="contain" />
+              <TouchableOpacity
+                style={{ marginTop: height * 0.02, borderRadius: 20, padding: width * 0.04, backgroundColor: 'white' }}
+                onPress={() => setImageModalVisible(!isImageModalVisible)}
+              >
+                <Text>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+          </View>
+          );
 }
 
 const styles = StyleSheet.create({
@@ -196,5 +326,33 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     textAlign: "center"
-  }
+  },
+  lineStyle: {
+    borderWidth: 0.5,
+    borderColor: 'black',
+    margin: 5,
+  },
+  postStats: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: height * 0.01,
+    marginTop: height * 0.02,
+  },
+  statsText: {
+    color: '#888',
+  },
+  commentContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
+  },
+  commentUser: {
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  commentText: {
+    flex: 1,
+  },
 });
